775ca10cd0aaf816afba7f2be031dfc1fd41e4fb
Sam Gunaratne <sam.gunaratne@uk.ibm.com>
2015-05-05 10:59:24 +0100

    Removed commented out code

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,13 +1,6 @@
 var React = require('../react/react.js');
 var TodoComponent = require('./components/TodoComponent.jsx')
 
-// window.app = (function(scope){
-//  return React.render(
-//      <BlogComponent />,
-//      document.getElementById('content')
-//  );
-// })();
-
 React.render(
         <TodoComponent url="todoData.json" />,
         document.getElementById('content')
